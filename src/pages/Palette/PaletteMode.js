5cc929fce851ce357ef2cf52c555ef8f31195645
Stephen McLean <smclean@SMCLEAN-MBP15.guidewire.com>
2019-06-18 09:07:16 +0100

    Allow selecting colors from the palette.

--- src/pages/Palette/PaletteMode.js
+++ src/pages/Palette/PaletteMode.js
@@ -1,4 +1,5 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
+import tinycolor from "tinycolor2";
 
 import styles from "./PaletteMode.module.scss";
 import { PaletteContext } from "../../components/PaletteProvider/PaletteProvider";
@@ -6,6 +7,42 @@ import ColorPaletteList from "../../components/ColorPaletteList/ColorPaletteList
 
 const PaletteMode = () => {
   const { palette } = useContext(PaletteContext);
+  const [selectedColors, setSelectedColors] = useState([]);
+
+  const onColorClick = color => {
+    const idx = selectedColors.findIndex(c => c.id === color.id);
+    const newSelectedColors = [...selectedColors];
+    if (idx > -1) {
+      newSelectedColors.splice(idx, 1);
+    } else {
+      newSelectedColors.push(color);
+    }
+
+    setSelectedColors(newSelectedColors);
+  };
+
+  const getCustomTileStyle = color => {
+    const idx = selectedColors.findIndex(c => c.id === ConvolverNode.id);
+    if (idx > -1) {
+      let variantComplement = tinycolor(color.color)
+        .complement()
+        .toHexString();
+
+      variantComplement = tinycolor
+        .mostReadable(color.color, [variantComplement], {
+          includeFallbackColors: true,
+          level: "AAA",
+          size: "large"
+        })
+        .toHexString();
+
+      return {
+        "--color-tile-border-color": variantComplement
+      };
+    }
+
+    return {};
+  };
 
   return (
     <div className={styles.container}>
@@ -17,6 +54,8 @@ const PaletteMode = () => {
             name={color.name}
             colors={allColors}
             className="margin-right--xs"
+            onColorClick={onColorClick}
+            getCustomTileStyle={getCustomTileStyle}
           />
         );
       })}
