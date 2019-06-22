de3de699a8bc407e622afcc6ce2e1f967167b07a
Stephen McLean <smclean@SMCLEAN-MBP15.guidewire.com>
2019-06-22 21:37:42 +0100

    Convert app styling to grid layout. Add footer to app

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import {
 } from "react-router-dom";
 import { ALL_ROUTES, VARIANT_ROUTE } from "./constants";
 import Nav from "./components/Nav/Nav";
+import Footer from "./components/Footer/Footer";
 import ModeSwitcher from "./components/ModeSwitcher/ModeSwitcher";
 import PaletteProvider from "./components/PaletteProvider/PaletteProvider";
 import styles from "./App.module.scss";
@@ -14,22 +15,26 @@ import styles from "./App.module.scss";
 function App() {
   return (
     <PaletteProvider>
-      <Nav />
+      <div className={styles.appContainer}>
+        <Nav />
 
-      <div className={styles.container}>
-        <Router>
-          <ModeSwitcher className="margin-bottom--lg" />
-          <Switch>
-            {ALL_ROUTES.map(route => (
-              <Route
-                key={route.name}
-                path={route.path}
-                component={route.component}
-              />
-            ))}
-            <Redirect from="/" to={VARIANT_ROUTE.path} />
-          </Switch>
-        </Router>
+        <div className={styles.container}>
+          <Router>
+            <ModeSwitcher className="margin-bottom--lg" />
+            <Switch>
+              {ALL_ROUTES.map(route => (
+                <Route
+                  key={route.name}
+                  path={route.path}
+                  component={route.component}
+                />
+              ))}
+              <Redirect from="/" to={VARIANT_ROUTE.path} />
+            </Switch>
+          </Router>
+        </div>
+
+        <Footer />
       </div>
     </PaletteProvider>
   );
