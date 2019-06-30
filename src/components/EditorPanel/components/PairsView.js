32b983d77d634caa6f429ec4c2ed6dc5b5abda9f
Stephen McLean <smclean@SMCLEAN-MBP15.guidewire.com>
2019-06-30 20:05:28 +0100

    Add placeholder to pairs view

--- src/components/EditorPanel/components/PairsView.js
+++ src/components/EditorPanel/components/PairsView.js
@@ -1,13 +1,31 @@
 import React from "react";
+import cx from "classnames";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import AccessiblePair from "../../AccessiblePair/AccessiblePair";
+
+import styles from "./PaletteView.module.scss";
+
+const PlaceHolder = () => {
+  const iconClass = cx(styles.icon, "margin-bottom--xxs");
+  return (
+    <div className={styles.placeholder}>
+      <FontAwesomeIcon icon="box" className={iconClass} />
+      <div>Drag colors here to create pairs</div>
+    </div>
+  );
+};
 
 const PairsView = ({ pairs }) => {
   return (
     <div>
-      {pairs.map(pair => (
-        <AccessiblePair {...pair} key={pair.id} className="margin-bottom" />
-      ))}
+      {pairs && pairs.length ? (
+        pairs.map(pair => (
+          <AccessiblePair {...pair} key={pair.id} className="margin-bottom" />
+        ))
+      ) : (
+        <PlaceHolder />
+      )}
     </div>
   );
 };
